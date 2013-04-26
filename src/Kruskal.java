8da62ecad5274a3217f95b9f1e1e81fcf0d8ed6f
reahm <dj.reah@gmail.com>
2013-04-26 01:58:48 -0700

    initial source given with project

---
+++ src/Kruskal.java
@@ -0,0 +1,19 @@
+/* Kruskal.java */
+
+import graph.*;
+import set.*;
+
+/**
+ * The Kruskal class contains the method minSpanTree(), which implements
+ * Kruskal's algorithm for computing a minimum spanning tree of a graph.
+ */
+
+public class Kruskal {
+
+  /**
+   * minSpanTree() returns a WUGraph that represents the minimum spanning tree
+   * of the WUGraph g.  The original WUGraph g is NOT changed.
+   */
+  public static WUGraph minSpanTree(WUGraph g);
+
+}
